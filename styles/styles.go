ba3feb4605f4f98603b08b00ce12044aa2a9eb88
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-04-09 22:07:48 +0200

    added new colors

--- styles/styles.go
+++ styles/styles.go
@@ -9,6 +9,18 @@ var (
 		ANSI:      "4",
 	}
 
+	errPrimary = lipgloss.CompleteColor{
+		TrueColor: "af0000",
+		ANSI256:   "124",
+		ANSI:      "1",
+	}
+
+	warningPrimary = lipgloss.CompleteColor{
+		TrueColor: "#d75f00",
+		ANSI256:   "166",
+		ANSI:      "11",
+	}
+
 	textPrimary = lipgloss.CompleteColor{
 		TrueColor: "#ffffaf",
 		ANSI256:   "229",
