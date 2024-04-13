f9543af672b87cfb7d19a8b0062cf50f9957d97c
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-04-13 15:35:26 +0200

    refactor: basic core model and views setup

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/help"
-	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"github.com/condemo/home-inventory/data"
-	"github.com/condemo/home-inventory/elements"
-	"github.com/condemo/home-inventory/keymaps"
-	"github.com/condemo/home-inventory/styles"
+	"github.com/condemo/home-inventory/screens"
 )
 
 type currentView int
@@ -23,21 +16,17 @@ const (
 )
 
 type Model struct {
-	help      help.Model
-	keys      keymaps.KeyMap
-	itemTable table.Model
-	loaded    bool
-	quitting  bool
+	viewList []tea.Model
+	focused  currentView
+	quitting bool
 }
 
-var store = data.InitDatabase()
+func NewModel() tea.Model {
+	m := Model{}
+	m.viewList = append(m.viewList, screens.NewMainModel())
+	m.focused = mainView
 
-func NewModel() *Model {
-	return &Model{
-		itemTable: elements.NewTable(store),
-		keys:      keymaps.AppKeys,
-		help:      help.New(),
-	}
+	return m
 }
 
 func (m Model) Init() tea.Cmd { return nil }
@@ -45,32 +34,8 @@ func (m Model) Init() tea.Cmd { return nil }
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.help.Width = msg.Width
-
 	case tea.KeyMsg:
-		switch {
-		case key.Matches(msg, m.keys.Quit):
-			m.quitting = true
-			return m, tea.Quit
-
-		case key.Matches(msg, m.keys.Down):
-			if m.itemTable.Cursor() == len(m.itemTable.Rows())-1 {
-				m.itemTable.GotoTop()
-			} else {
-				m.itemTable.SetCursor(m.itemTable.Cursor() + 1)
-			}
-
-		case key.Matches(msg, m.keys.Up):
-			if m.itemTable.Cursor() == 0 {
-				m.itemTable.SetCursor(len(m.itemTable.Rows()))
-			} else {
-				m.itemTable.SetCursor(m.itemTable.Cursor() - 1)
-			}
-
-		case key.Matches(msg, m.keys.Help):
-			m.help.ShowAll = !m.help.ShowAll
-		}
+		return m.viewList[m.focused].Update(msg)
 	}
 	return m, cmd
 }
@@ -79,15 +44,8 @@ func (m Model) View() string {
 	if m.quitting {
 		return ""
 	}
-
-	m.help.Styles.FullKey = styles.HelpStyle
-
-	helpView := m.help.View(m.keys)
-
-	mainContainer := lipgloss.JoinVertical(
-		lipgloss.Center, m.itemTable.View(), helpView)
-
-	return styles.ContainerStyle.Render(mainContainer)
+	s := m.viewList[m.focused].View()
+	return s
 }
 
 func main() {
---
+++ screens/mainView.go
@@ -0,0 +1,81 @@
+package screens
+
+import (
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/condemo/home-inventory/data"
+	"github.com/condemo/home-inventory/elements"
+	"github.com/condemo/home-inventory/keymaps"
+	"github.com/condemo/home-inventory/styles"
+)
+
+type MainView struct {
+	help      help.Model
+	keys      keymaps.KeyMap
+	itemTable table.Model
+	loaded    bool
+	quitting  bool
+}
+
+var store = data.InitDatabase()
+
+func NewMainModel() *MainView {
+	return &MainView{
+		itemTable: elements.NewTable(store),
+		keys:      keymaps.AppKeys,
+		help:      help.New(),
+	}
+}
+
+func (m MainView) Init() tea.Cmd { return nil }
+
+func (m MainView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
+
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keys.Quit):
+			m.quitting = true
+			return m, tea.Quit
+
+		case key.Matches(msg, m.keys.Down):
+			if m.itemTable.Cursor() == len(m.itemTable.Rows())-1 {
+				m.itemTable.GotoTop()
+			} else {
+				m.itemTable.SetCursor(m.itemTable.Cursor() + 1)
+			}
+
+		case key.Matches(msg, m.keys.Up):
+			if m.itemTable.Cursor() == 0 {
+				m.itemTable.SetCursor(len(m.itemTable.Rows()))
+			} else {
+				m.itemTable.SetCursor(m.itemTable.Cursor() - 1)
+			}
+
+		case key.Matches(msg, m.keys.Help):
+			m.help.ShowAll = !m.help.ShowAll
+		}
+	}
+	return m, cmd
+}
+
+func (m MainView) View() string {
+	if m.quitting {
+		return ""
+	}
+
+	m.help.Styles.FullKey = styles.HelpStyle
+
+	helpView := m.help.View(m.keys)
+
+	mainContainer := lipgloss.JoinVertical(
+		lipgloss.Center, m.itemTable.View(), helpView)
+
+	return styles.ContainerStyle.Render(mainContainer)
+}
