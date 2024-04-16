2d38516e869df75b144599f7aaea0bad1e35167e
Gustavo de los Santos <gustleo.dev@gmail.com>
2024-04-16 17:50:34 +0200

    refactor: complete input list  rebuild

--- screens/items.go
+++ screens/items.go
@@ -1,6 +1,8 @@
 package screens
 
 import (
+	"strings"
+
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textinput"
@@ -10,47 +12,57 @@ import (
 	"github.com/condemo/home-inventory/styles"
 )
 
+type inputsType int
+
+const (
+	nameInput inputsType = iota
+	amountInput
+	placeInput
+	tagsInput
+)
+
 type AddItemsView struct {
-	help        help.Model
-	keys        keymaps.ItemsKeymaps
-	nameInput   textinput.Model
-	amountInput textinput.Model
-	placeInput  textinput.Model
-	tagsInput   textinput.Model
-	quitting    bool
+	help     help.Model
+	keys     keymaps.ItemsKeymaps
+	inputs   []textinput.Model
+	quitting bool
 }
 
 func NewItemsView() *AddItemsView {
-	ni := textinput.New()
-	ni.Placeholder = "nombre"
-	ni.CharLimit = 25
-	ni.Width = 30
-
-	ai := textinput.New()
-	ai.Placeholder = "can."
-	ai.CharLimit = 4
-	ai.Width = 4
-
-	pi := textinput.New()
-	pi.Placeholder = "lugar"
-	pi.CharLimit = 100
-	pi.Width = 100
-
-	ti := textinput.New()
-	ti.Placeholder = "tags"
-	ti.CharLimit = 100
-	ti.Width = 100
-
-	ni.Focus()
-
-	return &AddItemsView{
-		help:        help.New(),
-		keys:        keymaps.ItemsKeys,
-		nameInput:   ni,
-		amountInput: ai,
-		placeInput:  pi,
-		tagsInput:   ti,
+	m := AddItemsView{
+		help:   help.New(),
+		keys:   keymaps.ItemsKeys,
+		inputs: make([]textinput.Model, 4),
 	}
+
+	for i := range m.inputs {
+		t := textinput.New()
+		t.Cursor.Style = styles.CursorSelectStyle
+		switch i {
+		case int(nameInput):
+			t.Placeholder = "nombre"
+			t.CharLimit = 25
+			t.Width = 30
+			t.PromptStyle = styles.InputFocusedStyle
+			t.TextStyle = styles.TextPrimaryStyle
+			t.Focus()
+		case int(amountInput):
+			t.Placeholder = "can."
+			t.CharLimit = 4
+			t.Width = 4
+		case int(placeInput):
+			t.Placeholder = "lugar"
+			t.CharLimit = 100
+			t.Width = 100
+		case int(tagsInput):
+			t.Placeholder = "tags"
+			t.CharLimit = 100
+			t.Width = 100
+		}
+		m.inputs[i] = t
+	}
+
+	return &m
 }
 
 func (m AddItemsView) Init() tea.Cmd { return nil }
@@ -67,25 +79,31 @@ func (m AddItemsView) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 	}
-	if m.nameInput.Focused() {
-		m.nameInput, cmd = m.nameInput.Update(msg)
+	if m.inputs[nameInput].Focused() {
+		m.inputs[nameInput], cmd = m.inputs[nameInput].Update(msg)
 	}
 	return m, cmd
 }
 
 func (m AddItemsView) View() string {
+	var b strings.Builder
+
 	if m.quitting {
 		return ""
+	}
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
 	}
 
 	helpView := m.help.View(m.keys)
 
 	mainContainer := lipgloss.JoinVertical(
 		lipgloss.Left,
-		m.nameInput.View(),
-		m.placeInput.View(),
-		m.amountInput.View(),
-		m.tagsInput.View(),
+		b.String(),
 		styles.HelpContainer.Render(helpView))
 
 	return styles.ContainerStyle.Render(mainContainer)
